Federico-PizarroBejarano/upright

commit b76d0590170b0a0edb6e09947ee8c497a3192edc

Make contact point reference to tray origin rather than object CoMs.
This change allows the inertial parameters of the objects to be changed
without the need to recompile the constraint derivative code.

upright_core/src/upright_core/parsing.py
@@ -192,7 +192,8 @@ def _parse_objects_with_contacts(wrappers, contact_conf, tol=1e-7):
 
             span = math.plane_span(normal)
 
-            r1 = points[i, :] - body1.com
+            # r1 = points[i, :] - body1.com
+            r1 = points[i, :]
 
             # it doesn't make sense to inset w.r.t. fixtures (EE or otherwise),
             # because we don't have to worry about their dynamics
@@ -207,7 +208,8 @@ def _parse_objects_with_contacts(wrappers, contact_conf, tol=1e-7):
                 # unproject the inset back into 3D space
                 r1_inset = r1 + (r1_t_inset - r1_t) @ span
 
-            r2 = points[i, :] - body2.com
+            # r2 = points[i, :] - body2.com
+            r2 = points[i, :]
             r2_t = span @ r2
             r2_t_inset = math.inset_vertex(r2_t, inset)
             r2_inset = r2 + (r2_t_inset - r2_t) @ span
@@ -250,59 +252,6 @@ def parse_box(obj_type_conf, position=None, rotation=None):
         box = polyhedron.ConvexPolyhedron.box(local_half_extents)
 
     return box.transform(translation=position, rotation=rotation)
-
-
-def compute_support_area(box, parent_box, com_offset, inset, tol=1e-6):
-    """Compute the support area.
-
-    Currently only a rectangular support area is supported, due to the need to
-    compute a value for r_tau.
-
-    Parameters:
-        box: the collision box of the current object
-        parent_box: the collision box of the parent
-        inset: positive scalar denoting how much to reduce the support area
-
-    Returns:
-        support area, r_tau
-    """
-    # NOTE this approach is not completely general for cylinders, where we may
-    # want contacts with more than just their boxes
-    support_points, normal = polyhedron.axis_aligned_contact(box, parent_box)
-    assert inset >= 0, "Support area inset must be non-negative."
-
-    # check all points are coplanar:
-    assert np.all(np.abs((support_points - support_points[0, :]) @ normal) < tol)
-
-    # r_tau assumes rectangular for now, check:
-    n = support_points.shape[0]
-    assert n == 4, f"Support area has {n} points, not 4!"
-    lengths = []
-    for i in range(-1, 3):
-        lengths.append(np.linalg.norm(support_points[i + 1, :] - support_points[i, :]))
-    assert (
-        abs(lengths[0] - lengths[2]) < tol and abs(lengths[1] - lengths[3]) < tol
-    ), f"Support area is not rectangular!"
-
-    # translate points to be relative to the CoM position
-    com_position = box.position + com_offset
-    local_support_points = support_points - com_position
-
-    # project points into the tangent plane
-    span = math.plane_span(normal)
-    local_support_points_t = (span @ local_support_points.T).T
-
-    # apply inset
-    local_support_points_t_inset = np.zeros_like(local_support_points_t)
-    for i in range(local_support_points_t.shape[0]):
-        local_support_points_t_inset[i, :] = math.inset_vertex(
-            local_support_points_t[i, :], inset
-        )
-        # local_points_xy[i, :] = math.inset_vertex_abs(local_points_xy[i, :], inset)
-
-    support_area = PolygonSupportArea(list(local_support_points_t_inset), normal, span)
-    r_tau = math.rectangle_r_tau(lengths[0], lengths[1])
-    return support_area, r_tau
 
 
 def parse_mu_dict(contact_conf, apply_margin):

upright_robust/src/upright_robust/parsing.py
@@ -39,12 +39,12 @@ def parse_objects_and_contacts(
 
     # make EE origin the reference point for all contacts
     bodies = model.settings.balancing_settings.bodies
-    for c in model.settings.balancing_settings.contacts:
-        if c.object1_name != "ee":
-            b1 = bodies[c.object1_name]
-            c.r_co_o1 = c.r_co_o1 + b1.com
-        b2 = bodies[c.object2_name]
-        c.r_co_o2 = c.r_co_o2 + b2.com
+    # for c in model.settings.balancing_settings.contacts:
+    #     if c.object1_name != "ee":
+    #         b1 = bodies[c.object1_name]
+    #         c.r_co_o1 = c.r_co_o1 + b1.com
+    #     b2 = bodies[c.object2_name]
+    #     c.r_co_o2 = c.r_co_o2 + b2.com
 
     # update friction coefficient if one was passed in
     contacts = []

upright_core/include/upright_core/contact_constraints.h
@@ -105,6 +105,7 @@ Wrench<Scalar> compute_object_dynamics_constraint(
 // points.
 template <typename Scalar>
 std::map<std::string, Wrench<Scalar>> compute_object_wrenches(
+    const std::map<std::string, RigidBody<Scalar>>& bodies,
     const std::vector<ContactPoint<Scalar>>& contacts,
     const VecX<Scalar>& forces) {
     const bool frictionless = (forces.size() == contacts.size());
@@ -119,27 +120,37 @@ std::map<std::string, Wrench<Scalar>> compute_object_wrenches(
         }
 
         // TODO this relies on object frame having same orientation as EE frame
-        if (object_wrenches.find(contact.object1_name) ==
-            object_wrenches.end()) {
-            object_wrenches[contact.object1_name].force = f;
-            object_wrenches[contact.object1_name].torque =
-                contact.r_co_o1.cross(f);
-        } else {
-            object_wrenches[contact.object1_name].force += f;
-            object_wrenches[contact.object1_name].torque +=
-                contact.r_co_o1.cross(f);
+
+        // The first object may be the EE, which we do not store in the bodies
+        // map because we do not need to compute a wrench or constraints for it
+        if (bodies.find(contact.object1_name) != bodies.end()) {
+            Vec3<Scalar> com1 = bodies.at(contact.object1_name).com;
+            Vec3<Scalar> lever1 = contact.r_co_o1 - com1;
+
+            if (object_wrenches.find(contact.object1_name) ==
+                object_wrenches.end()) {
+                object_wrenches[contact.object1_name].force = f;
+                object_wrenches[contact.object1_name].torque = lever1.cross(f);
+            } else {
+                object_wrenches[contact.object1_name].force += f;
+                object_wrenches[contact.object1_name].torque += lever1.cross(f);
+            }
         }
 
-        // For the second object, the forces are negative
-        if (object_wrenches.find(contact.object2_name) ==
-            object_wrenches.end()) {
-            object_wrenches[contact.object2_name].force = -f;
-            object_wrenches[contact.object2_name].torque =
-                contact.r_co_o2.cross(-f);
-        } else {
-            object_wrenches[contact.object2_name].force -= f;
-            object_wrenches[contact.object2_name].torque +=
-                contact.r_co_o2.cross(-f);
+        {
+            Vec3<Scalar> com2 = bodies.at(contact.object2_name).com;
+            Vec3<Scalar> lever2 = contact.r_co_o2 - com2;
+
+            // For the second object, the forces are negative
+            if (object_wrenches.find(contact.object2_name) ==
+                object_wrenches.end()) {
+                object_wrenches[contact.object2_name].force = -f;
+                object_wrenches[contact.object2_name].torque = lever2.cross(-f);
+            } else {
+                object_wrenches[contact.object2_name].force -= f;
+                object_wrenches[contact.object2_name].torque +=
+                    lever2.cross(-f);
+            }
         }
     }
     return object_wrenches;
@@ -153,8 +164,8 @@ VecX<Scalar> compute_object_dynamics_constraints(
     const std::vector<ContactPoint<Scalar>>& contacts,
     const VecX<Scalar>& forces, const RigidBodyState<Scalar>& state,
     const Vec3<Scalar>& gravity) {
-    std::map<std::string, Wrench<Scalar>> object_wrenches =
-        compute_object_wrenches(contacts, forces);
+    const std::map<std::string, Wrench<Scalar>> object_wrenches =
+        compute_object_wrenches(bodies, contacts, forces);
 
     VecX<Scalar> constraints(NUM_DYNAMICS_CONSTRAINTS_PER_OBJECT *
                              bodies.size());
@@ -170,7 +181,7 @@ VecX<Scalar> compute_object_dynamics_constraints(
         auto& name = kv.first;
         auto& body = kv.second;
 
-        Wrench<Scalar> wrench = object_wrenches[name];
+        Wrench<Scalar> wrench = object_wrenches.at(name);
         Wrench<Scalar> constraint = compute_object_dynamics_constraint(
             body, wrench, state, gravity, force_scale);
         constraints.segment(i * NUM_DYNAMICS_CONSTRAINTS_PER_OBJECT,

upright_robust/config/demos/sim.yaml
@@ -4,6 +4,7 @@ include:
     path: config/demos/_base.yaml
 
 simulation:
+  timestep: 0.0001
   robot:
     urdf:
       args:

upright_robust/scripts/planning_sim_loop.py
@@ -32,6 +32,7 @@ def run_simulation(config, video, logname, use_gui=True):
     # start the simulation
     print("starting sim")
     timestamp = datetime.datetime.now()
+    # sim_config["timestep"] = 0.00001
     env = sim.simulation.UprightSimulation(
         config=sim_config,
         timestamp=timestamp,
@@ -96,6 +97,12 @@ def run_simulation(config, video, logname, use_gui=True):
     mpc.advanceMpc()
     print("Ready to start.")
 
+    # compute initial object offset from tray
+    r_ew_w, Q_we = env.robot.link_pose()
+    r_ow_ws, Q_wos = env.object_poses()
+    C_we = core.math.quat_to_rot(Q_we)
+    r_oe_e0 = C_we.T @ (r_ow_ws[0] - r_ew_w)
+
     # simulation loop
     step = 0
     while t <= env.duration:
@@ -136,7 +143,7 @@ def run_simulation(config, video, logname, use_gui=True):
         env.robot.command_velocity(v_cmd, bodyframe=False)
 
         # TODO more logger reforms to come
-        if step % 10 == 0:
+        if step % 100 == 0:
             # log sim stuff
             r_ew_w, Q_we = env.robot.link_pose()
             v_ew_w, ω_ew_w = env.robot.link_velocity()
@@ -160,6 +167,12 @@ def run_simulation(config, video, logname, use_gui=True):
             logger.append("Q_we_ds", Q_we_d)
 
             logger.append("solve_times", ctrl_manager.mpc.getLastSolveTime())
+
+            # compute current object offset from tray
+            C_we = core.math.quat_to_rot(Q_we)
+            r_oe_e = C_we.T @ (r_ow_ws[0] - r_ew_w)
+            obj_err = np.linalg.norm(r_oe_e - r_oe_e0)
+            # print(f"obj_err = {obj_err}")
 
             if model.settings.balancing_settings.enabled:
                 model.update(x, u)
@@ -379,7 +392,7 @@ def main():
     )
     parser.add_argument(
         "--com",
-        choices=["center", "top", "bottom", "robust"],
+        choices=["center", "top", "bottom", "robust", "exact"],
         required=True,
         help="Where the controller should put the CoM.",
     )
@@ -392,6 +405,7 @@ def main():
     args = parser.parse_args()
 
     use_robust = args.com == "robust"
+    use_exact = args.com == "exact"
 
     # load configuration
     master_config = core.parsing.load_config(args.config)
@@ -402,12 +416,11 @@ def main():
     h_cm = args.height
     h_m = args.height / 100
     h2_m = 0.5 * h_m
-    b = 0.06  # 0.04
+    b = 0.06  # 12cm x 12cm bounding box for the CoM
 
     ctrl_obj_config = {
         "mass": 1.0,
         "shape": "cuboid",
-        # "side_lengths": [0.1, 0.1, h_m],
         "side_lengths": [0.15, 0.15, h_m],
         "color": [1, 0, 0, 1],
         "bounds": {
@@ -442,7 +455,9 @@ def main():
 
     # build the nominal arrangement
     # this is even built in the robust case for later post-processing
-    x_offset = 0
+    # NOTE: we do not require a deepcopy here unless we were changing values in
+    # the nested dicts (i.e., the bounds), or directly modifying elements of
+    # the nested lists
     nom_ctrl_obj_config = ctrl_obj_config.copy()
     if args.com == "center" or use_robust:
         nom_ctrl_obj_config["com_offset"] = [0, 0, 0]
@@ -450,8 +465,11 @@ def main():
         nom_ctrl_obj_config["com_offset"] = [0, 0, -float(ctrl_com_box.half_extents[2])]
     elif args.com == "top":
         nom_ctrl_obj_config["com_offset"] = [0, 0, float(ctrl_com_box.half_extents[2])]
+    elif not use_exact:
+        raise ValueError("unexpected value for --com")
     master_config["controller"]["objects"][OBJECT_NAME] = nom_ctrl_obj_config
 
+    x_offset = 0
     nom_ctrl_arrangement_config = make_arrangement_config(
         [OBJECT_NAME], x_offset=x_offset, mu=MU
     )
@@ -468,10 +486,6 @@ def main():
 
         # add to the main config
         for name, com in zip(object_names, ctrl_com_offsets):
-            # params_com = params_center.transform(translation=com)
-            # IPython.embed()
-            # return
-
             config = ctrl_obj_config.copy()
             config["com_offset"] = com.tolist()
             master_config["controller"]["objects"][name] = config
@@ -501,7 +515,7 @@ def main():
         + box_face_centers(sim_com_box)
         + [list(v) for v in sim_com_box.vertices]
     )
-    max_com_z_offset = np.max(sim_com_offsets, axis=0)[2]
+    # max_com_z_offset = np.max(sim_com_offsets, axis=0)[2]
 
     # mass-normalized inertias (about the CoM)
     sim_inertias_diag = [
@@ -523,12 +537,21 @@ def main():
                     {"time": 0, "position": waypoint, "orientation": [0, 0, 0, 1]}
                 ]
 
+                # NOTE: no need to copy since we update every changing field
+                # for each run
                 sim_obj_config["com_offset"] = np.array(com_offset).tolist()
                 sim_obj_config["inertia_diag"] = (s * inertia_diag).tolist()
                 config["simulation"]["objects"][OBJECT_NAME] = sim_obj_config
 
+                # when using the exact inertial parameters, we need to update
+                # them for each run
+                if use_exact:
+                    nom_ctrl_obj_config["com_offset"] = np.array(com_offset).tolist()
+                    nom_ctrl_obj_config["inertia"] = (s * inertia_diag).tolist()
+                    config["controller"]["objects"][OBJECT_NAME] = nom_ctrl_obj_config
+
                 # inspect a particular run
-                # if run != 70:
+                # if run != 30:
                 #     run += 1
                 #     continue
                 # IPython.embed()
@@ -536,6 +559,9 @@ def main():
                 # only compile at most once
                 if run > 1:
                     config["controller"]["recompile_libraries"] = False
+
+                # TODO recompiling the constraints does not work properly!!
+                # config["controller"]["recompile_libraries"] = True
 
                 if args.log:
                     name = f"{dirname}/run_{run}"

commit 5d9d99155b52861a2e642067bf2ed676613432eb

Squashed commit of the following:
commit 48935f1f2e6f8723cdad1d614a892722249db150
Author: Adam Heins <mail@adamheins.com>
Date:   Thu Jul 24 16:51:46 2025 -0400

    Fix tests.

    Tests have been broken since commit
    b76d0590170b0a0edb6e09947ee8c497a3192edc, when the reference point for
    objects was changed from the CoM to a shared body frame origin.

commit e3bf9c29281fd4ba783492db25d140287db33065
Merge: a35cb37 85dddef
Author: Adam Heins <mail@adamheins.com>
Date:   Thu Jul 24 15:38:07 2025 -0400

    Merge pull request #11 from utiasDSL/fed

    Minor fixes

upright_core/src/upright_core/parsing.py
@@ -190,7 +190,6 @@ def _parse_objects_with_contacts(wrappers, contact_conf, tol=1e-7):
 
             span = math.plane_span(normal)
 
-            # r1 = points[i, :] - body1.com
             r1 = points[i, :]
 
             # it doesn't make sense to inset w.r.t. fixtures (EE or otherwise),
@@ -199,16 +198,16 @@ def _parse_objects_with_contacts(wrappers, contact_conf, tol=1e-7):
                 r1_inset = r1
             else:
                 # project point into tangent plane and inset the tangent part
+                # wrt to the box's center
                 # NOTE this does not make sense for non-planar contacts
-                r1_t = span @ r1
+                r1_t = span @ (r1 - box1.position)
                 r1_t_inset = math.inset_vertex(r1_t, inset)
 
                 # unproject the inset back into 3D space
                 r1_inset = r1 + (r1_t_inset - r1_t) @ span
 
-            # r2 = points[i, :] - body2.com
             r2 = points[i, :]
-            r2_t = span @ r2
+            r2_t = span @ (r2 - box2.position)
             r2_t_inset = math.inset_vertex(r2_t, inset)
             r2_inset = r2 + (r2_t_inset - r2_t) @ span
 

upright_core/src/upright_core/util.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from upright_core import math
 
 
@@ -11,12 +12,6 @@ def sort_canonical(A):
 
 
 def allclose_unordered(A, B):
-    """Helper to compare two nd-arrays where each array should have the same
-    rows, but they may be in different orders.
-
-    Returns True if the arrays are the same (but possibly with rows in a
-    different order), False otherwise.
-    """
     assert A.shape == B.shape
     n = A.shape[0]
     B_checked = np.zeros(n, dtype=bool)

upright_core/tests/test_parsing.py
@@ -101,15 +101,14 @@ def test_parse_box():
     points1 = np.array([c.r_co_o1 for c in contacts])
     points2 = np.array([c.r_co_o2 for c in contacts])
 
-    points1_expected = np.array(
-        [[0.1, 0.1, 0.01], [0.1, -0.1, 0.01], [-0.1, -0.1, 0.01], [-0.1, 0.1, 0.01]]
-    )
-    points2_expected = np.array(
-        [[0.1, 0.1, -0.1], [0.1, -0.1, -0.1], [-0.1, -0.1, -0.1], [-0.1, 0.1, -0.1]]
+    points_expected = np.array(
+        [[0.1, 0.1, 0], [0.1, -0.1, 0], [-0.1, -0.1, 0], [-0.1, 0.1, 0]]
     )
 
-    assert allclose_unordered(points1, points1_expected)
-    assert allclose_unordered(points2, points2_expected)
+    # both sets of points should be the same since we use a common reference
+    # point
+    assert allclose_unordered(points1, points_expected)
+    assert allclose_unordered(points2, points_expected)
 
 
 def test_parse_cylinder_box():
@@ -131,14 +130,14 @@ def test_parse_cylinder_box():
 
     # fmt: off
     ee_points_expected = np.array([
-        [0, 0, 0.01], [-0.03, 0.03, 0.01], [-0.06, 0, 0.01], [-0.03, -0.03, 0.01],
-        [0, -0.1, 0.01], [0.2, -0.1, 0.01], [0.2, 0.1, 0.01], [0, 0.1, 0.01]])
+        [0, 0, 0], [-0.03, 0.03, 0], [-0.06, 0, 0], [-0.03, -0.03, 0],  # cylinder
+        [0, -0.1, 0], [0.2, -0.1, 0], [0.2, 0.1, 0], [0, 0.1, 0]])  # box
     box_points_expected = np.array([
-        [-0.1, -0.1, -0.1], [0.1, -0.1, -0.1], [0.1, 0.1, -0.1], [-0.1, 0.1, -0.1],
-        [-0.1, 0, -0.1], [-0.1, 0, 0.1]])
+        [0, -0.1, 0], [0.2, -0.1, 0], [0.2, 0.1, 0], [0, 0.1, 0],
+        [0, 0, 0], [0, 0, 0.2]])
     cylinder_points_expected = np.array([
-        [0.03, 0, -0.15], [0, 0.03, -0.15], [-0.03, 0, -0.15], [0, -0.03, -0.15],
-        [0.03, 0, -0.15], [0.03, 0, 0.05]])
+        [0, 0, 0], [-0.03, 0.03, 0], [-0.06, 0, 0], [-0.03, -0.03, 0],
+        [0, 0, 0], [0, 0, 0.2]])
     # fmt: on
 
     assert allclose_unordered(points["ee"], ee_points_expected)
@@ -172,11 +171,11 @@ def test_parse_wedge_box():
 
     # fmt: off
     wedge_points_expected = np.array([
-        [0.2, 0.15, -0.1], [0.2, -0.15, -0.1], [-0.1, -0.15, -0.1], [-0.1, 0.15, -0.1],
-        [0.05, 0.1, 0.05], [0.05, -0.1, 0.05], [0.05 - a, 0.1, 0.05 + a],
-        [0.05 - a, -0.1, 0.05 + a]])
+        [0.15, 0.15, 0], [0.15, -0.15, 0], [-0.15, -0.15, 0], [-0.15, 0.15, 0],
+        [0, 0.1, 0.15], [0, -0.1, 0.15], [-a, 0.1, 0.15 + a],
+        [-a, -0.1, 0.15 + a]])
     box_points_expected = np.array([
-        [0, 0.1, -a], [0, -0.1, -a], [-a, -0.1, 0], [-a, 0.1, 0]])
+        [0, 0.1, 0.15], [0, -0.1, 0.15], [-a, -0.1, 0.15 + a], [-a, 0.1, 0.15 + a]])
     # fmt: on
 
     assert allclose_unordered(points["wedge"], wedge_points_expected)